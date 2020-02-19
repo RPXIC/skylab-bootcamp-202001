82754715d8a6a22c194288eac6e4677d1a69b8a3
RPXIC <rpc2works@gmail.com>
2020-02-19 15:30:29 +0100

    chat modifi

--- staff/ruben-ponce/chat/chat-client.js
+++ staff/ruben-ponce/chat/chat-client.js
@@ -1,8 +1,27 @@
+// const net = require('net')
+
+// const { argv: [, , message, host, port] } = process
+
+// const socket = net.connect({ host, port })
+
+// socket.write(message)
+
 const net = require('net')
-
-const { argv: [, , message, host, port] } = process
-
-const socket = net.connect({ host, port })
-
-socket.write(message)
-
+const readline = require('readline')
+// $ node client <nick> <ip> <port>
+const { argv: [, , nick, ip, port] } = process
+const socket = net.createConnection({ host: ip, port })
+socket.write(nick)
+socket.on('data', chunk => {
+    console.log(chunk.toString())
+})
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+(function ask() {
+    rl.question('Send messsage to? ', userMessage => {
+        socket.write(userMessage)
+        ask()
+    })
+})()
--- staff/ruben-ponce/chat/chat-server.js
+++ staff/ruben-ponce/chat/chat-server.js
@@ -1,14 +1,38 @@
+// const net = require('net')
+// var sockets = []
+// const port = 8000
+// const server = net.createServer(socket => {
+
+//     socket.write(`Welcome to rpc chat server, Mr.${socket.remoteAddress.split(':')[3]}\n`);
+
+//     socket.on('data', chunk => {
+
+//         console.log(chunk.toString())
+
+//     })
+// })
+// server.listen(port)
+
 const net = require('net')
-var sockets = []
-const port = 8080
-const server = net.createServer(socket => {
-
-    socket.write(`Welcome to rpc chat server, Mr.${socket.remoteAddress.split(':')[3]}\n`);
-
-    socket.on('data', chunk => {
-
-        console.log(chunk.toString())
-
+const sessions = {}
+const server = net.createServer(socket => { debugger
+    socket.on('data', chunk => { // <user> or <user>:<message>
+        const alreadyExists = (() => {
+            for (const user in sessions)
+                if (socket === sessions[user]) {
+                    return true
+                }
+            return false
+        })()
+        debugger
+        if (!alreadyExists)
+            sessions[chunk.toString()] = socket
+        else {
+            const [user, message] = chunk.toString().split(':')
+            const _socket = sessions[user]
+            if (_socket) _socket.write(message)
+            else socket.write('ERROR user is not online')
+        }
     })
 })
-server.listen(port)
+server.listen(8000)
